5c8fc2609da09868cb3d7ccb6edd2250a1e9afe8
Delasmanos <info@delasmanos.de>
2023-11-17 12:20:37 +0100

    chore: try publish

--- src/pages/_rss.xml.js
+++ src/pages/_rss.xml.js
@@ -4,13 +4,21 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    iems:[
+      {title: 'bernd',
+      pubDat: '2323-11-11',
+      link: '/'
+    }
+    ]
+    // items: posts.map((post) => ({
+    //   tite: post.data.title,
+    //   pubDate: post.data.pubDate,
+    //   link: `/blog/${post.slug}/`,
+    // })),
   });
 }
