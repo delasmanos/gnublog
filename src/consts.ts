6e3f085e24464d1c66b23784c000f2e6c3460d00
Delasmanos <info@delasmanos.de>
2023-11-17 12:20:37 +0100

    fix: prefix link

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "GNU.band";
+// needed to work on github pages
+export const LINK_PREFIX = "gnublog";
 export const SITE_DESCRIPTION = "We are GNU a band from Berlin";
