ef5d9787dabca5f69c5ddf486425cd00655af2bb
Delasmanos <info@delasmanos.de>
2023-11-17 12:20:39 +0100

    feat: default filter collection helper, remove comments

--- src/util/collection-helper.ts
+++ src/util/collection-helper.ts
@@ -8,15 +8,16 @@ export const getPublishedPosts: GetPublishedPostFn = async (
   sortByDate = "latest-first",
   filter,
 ) =>
-  (
-    await getCollection("blog", (p) => {
-      if (filter) {
-        return p.data.published && filter(p);
-      }
-      return p.data.published;
-    })
-  ).sort((a, b) =>
+  (await getCollection("blog", (p) => defaultFilter(p, filter))).sort((a, b) =>
     sortByDate === "latest-first"
       ? b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
       : a.data.pubDate.valueOf() - b.data.pubDate.valueOf(),
   );
+type filter = (entry: CollectionEntry<"blog">, f?: filter) => unknown;
+const defaultFilter: filter = (entry, f) => {
+  const baseCondition = import.meta.env.PROD ? entry.data.published : true;
+  if (f) {
+    return baseCondition && f(entry);
+  }
+  return baseCondition;
+};
